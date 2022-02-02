8118e8e64d6c77143b5bb589623fa1f695e3ce86
Arslan Goraya <gorayaa66@gmail.com>
2022-02-02 17:24:27 +0500

    Updates

---
+++ precache-manifest.51132fd2fc3e626d743e5de2040c06e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104bd3d5128e4924f8054ec148181572",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "3deefd947e07ca18f990",
+    "url": "/build/static/css/main.c7964f61.chunk.css"
+  },
+  {
+    "revision": "09817d5f66efdebd2129",
+    "url": "/build/static/js/2.de93dab9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.de93dab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3deefd947e07ca18f990",
+    "url": "/build/static/js/main.9fe79f46.chunk.js"
+  },
+  {
+    "revision": "e9b16df5204316edaac9",
+    "url": "/build/static/js/runtime-main.09fae1e6.js"
+  }
+]);
